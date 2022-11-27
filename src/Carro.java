967a25611f667146a9a9ddc5f7d80122d3262a26
longArthur <44927382+longArthur@users.noreply.github.com>
2022-11-27 20:00:30 -0300

    VeiculoAutomotor.java modificado

--- src/Carro.java
+++ src/Carro.java
@@ -4,131 +4,59 @@ public class Carro extends VeiculoAutomotor{
 
     //atributos
     private int marcha;
-    private final int limiteMarca;
+    private final int limiteMarcha;
 
     //construtores
     public Carro(String marca, String modelo, LocalDate fabricacao, int limiteMarcha) {
         super(marca, modelo, fabricacao);
+        if (limiteMarcha < 0) throw new IllegalArgumentException("limite de marcaha nao pode ser negativo");
 
         this.marcha = 0;
-        this.limiteMarca = limiteMarcha;
+        this.limiteMarcha = limiteMarcha;
     }
 
     //getters
-    public int getMarcha(){ return this.marcha; }
+    public int getMarcha(){
+        return this.marcha;
+    }
 
-    public int getLimiteMarca(){ return this.limiteMarca; }
+    public int getLimiteMarcha(){
+        return this.limiteMarcha;
+    }
 
     //metodos
     @Override
     public boolean liga(){
-
-        if(!super.isLigado() && this.marcha == 0) {
-
-            super.liga();
-
-            return super.isLigado();
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean desliga(){
-
-        if(this.isLigado() && this.marcha == 0) {
-
-            super.desliga();
-
-            return super.isLigado();
-        }
-
-        return false;
+        if(this.marcha == 0)
+            return super.liga();
+        return this.isLigado();
     }
 
     @Override
     public boolean aumentaVelocidade(double aAdicionar) {
-
         double limiteMax = this.marcha * 20;
-
-        if (super.isLigado() && this.marcha > 0) {
-
-            if (aAdicionar < 0) {
-                return false;
-            }
-
-            if (super.getVelocidade() + aAdicionar > limiteMax) {
-
-                super.aumentaVelocidade(limiteMax - super.getVelocidade());
-
-                return true;
-            }
-
-            else {
-
-                super.aumentaVelocidade(aAdicionar);
-
-                    return true;
-            }
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean diminuiVelocidade(double aDiminuir){
-
-        if(super.isLigado() && super.getVelocidade() > 0){
-
-            if(aDiminuir < 0){
-
-                return false;
-            }
-
-            if(super.getVelocidade() - aDiminuir < 0){
-
-                super.diminuiVelocidade(super.getVelocidade());
-
-                    return true;
-
-            }
-
-            else {
-
-                super.diminuiVelocidade(aDiminuir);
-
-                return true;
-            }
-        }
-
-        return false;
+        if (this.getVelocidade() + aAdicionar >= limiteMax)
+            return super.aumentaVelocidade(limiteMax - this.getVelocidade());
+        else
+            return this.aumentaVelocidade(aAdicionar);
     }
 
     public void aumentaMarcha(int aMarcha){
-
-        if(this.marcha < this.limiteMarca) {
-
+        if(this.marcha < this.limiteMarcha)
             this.marcha++;
-        }
     }
 
     public void diminuiMarcha(int aMarcha){
-
-        if(this.marcha > 0){
-
+        if(this.marcha > 0)
             this.marcha--;
-        }
+        this.diminuiVelocidade(this.getVelocidade() - this.marcha * 20);
     }
+
     @Override
     public String toString() {
         return String.format("""
-                Marca: %s
-                Modelo: %s
-                Fabricacao: %d/%s/%d
-                Velocidade: %f02.1
-                Ligado: %b
-                Marchas: %d""", super.getMarca(), super.getModelo(),
-                super.getFabricacao().getDayOfMonth(), super.getFabricacao().getMonth(), super.getFabricacao().getYear(),
-                super.getVelocidade(), super.isLigado(), this.getLimiteMarca());
+                %s
+                Marchas: %d
+                Marcha atual: %d""", super.toString(), this.getLimiteMarcha(), this.getMarcha());
     }
 }
