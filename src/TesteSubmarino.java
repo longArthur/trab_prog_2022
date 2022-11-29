4be2a9b7862d0d9424ee559af24738e34a9ed35d
longArthur <44927382+longArthur@users.noreply.github.com>
2022-11-28 21:18:13 -0300

    TesteSubmarino.java feito

--- src/TesteSubmarino.java
+++ src/TesteSubmarino.java
@@ -1,5 +1,6 @@
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class TesteSubmarino {
     public static void main(String[] args) {
@@ -73,11 +74,35 @@ public class TesteSubmarino {
                 Profundidade: 0.0
                 Lim profundidade: 300.0"""));
 
+        Submarino teste2 = new Submarino("Ford", "Jabiraca",LocalDate.of(1999,12,30),"A3C-1234",29,3);
+        Submarino teste3 = new Submarino("Ford", "Jabiraca",LocalDate.of(1929,12,30),"A3C-1234",29,3);
+        Submarino teste4 = new Submarino("Ford", "jaca",LocalDate.of(1999,12,30),"A3C-1234",29,3);
+        Submarino teste5 = new Submarino("toyota", "Jabiraca",LocalDate.of(1999,12,30),"A3C-1234",29,3);
+        Submarino teste6 = new Submarino("Ford", "Jabiraca",LocalDate.of(1999,12,30),"A39-1234",9,32222);
+        System.out.println(!teste.equals(teste2));
+        System.out.println(!teste.equals(teste3));
+        System.out.println(!teste.equals(teste4));
+        System.out.println(!teste.equals(teste5));
+        System.out.println(teste2.equals(teste6));
 
-
-        //TODO public boolean equals(Object obj) {}
-
-        //TODO public int compareTo(VeiculoAutomotor obj){}
+        ArrayList<VeiculoAutomotor> arraySorteado = new ArrayList<>();
+        arraySorteado.add(teste5);
+        arraySorteado.add(teste3);
+        arraySorteado.add(teste2);
+        arraySorteado.add(teste6);
+        arraySorteado.add(teste);
+        arraySorteado.add(teste6);
+        arraySorteado.add(teste4);
+        ArrayList<VeiculoAutomotor> array = new ArrayList<>();
+        array.add(teste2);
+        array.add(teste6);
+        array.add(teste3);
+        array.add(teste);
+        array.add(teste5);
+        array.add(teste6);
+        array.add(teste4);
+        array.sort(null);
+        System.out.println(array.equals(arraySorteado));
 
     }
 }
